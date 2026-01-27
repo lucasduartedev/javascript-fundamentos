ae0671405f2e634a282c0cbb36b348ddb2b0bce1
Lucas Duarte <lucasduartedev@gmail.com>
2026-01-27 16:39:26 -0300

    Correção teste classe Conta

--- src/tests/models/banco/Conta.spec.ts
+++ src/tests/models/banco/Conta.spec.ts
@@ -1,12 +1,19 @@
 import Cartao from "../../../models/banco/Cartao";
 import Conta from "../../../models/banco/Conta"
 import {log, error} from "console"
+import PessoaFisica from "../../../models/banco/PessoaFisica";
+import Contato from "../../../models/banco/Contato";
 
 describe('Suite - Banco', () => {
 
     // * Conta
-    const conta01 = new Conta('0001');
-    const contaDesativada = new Conta('desativada_01');
+    const contato01 = new Contato('62999887722', 'fulano01@mail.com');
+    const pessoa01 = new PessoaFisica('Fulano de Tal-01', '123123123-45', contato01);
+    const conta01 = new Conta('0001', pessoa01);
+    
+    const contato02 = new Contato('62966117722', 'fulano02@mail.com');
+    const pessoa02 = new PessoaFisica('Fulano de Tal-02', '123123444-46', contato02);
+    const contaDesativada = new Conta('desativada_01', pessoa02);
     contaDesativada.depositar(1000);
     contaDesativada.desativar();
 
@@ -39,13 +46,17 @@ describe('Suite - Banco', () => {
     });
 
     // * Status
-    const contaTestStatusSaldo = new Conta('0147');
+    const contato03 = new Contato('62966119900', 'fulano03@mail.com');
+    const pessoa03 = new PessoaFisica('Fulano de Tal-03', '123566799-46', contato03);
+    const contaTestStatusSaldo = new Conta('0147', pessoa03);
     
     test('Deve retornar status POSITIVO com saldo igual ou maior zero (0).', () => {
         expect(contaTestStatusSaldo.status()).toContain('Positivo');
     });
     
-    const contaTestStatusSaldo2 = new Conta('0148');
+    const contato04 = new Contato('629551119900', 'fulano04@mail.com');
+    const pessoa04 = new PessoaFisica('Fulano de Tal-04', '123566799-46', contato04);
+    const contaTestStatusSaldo2 = new Conta('0148', pessoa04);
     contaTestStatusSaldo2.saldo = -1500
     test('Deve retornar status NEGATIVO com saldo menor que zero (0).', () => {
         expect(contaTestStatusSaldo2.status()).toContain('Negativo');
@@ -58,10 +69,20 @@ describe('Suite - Banco', () => {
     // ! checar final da string: 'status'
 
     // * Ativar/Desativar conta
-    const contaTestAtivo1 = new Conta('D0101');
-    const contaTestAtivo2 = new Conta('D0202');
+    const contato05 = new Contato('629551119900', 'fulano05@mail.com');
+    const pessoa05 = new PessoaFisica('Fulano de Tal-05', '123511799-46', contato05);
+    
+    const contato06 = new Contato('629551117900', 'fulano06@mail.com');
+    const pessoa06 = new PessoaFisica('Fulano de Tal-06', '123533799-46', contato06);
+    
+    const contato07 = new Contato('629551114500', 'fulano07@mail.com');
+    const pessoa07 = new PessoaFisica('Fulano de Tal-07', '123544799-46', contato07);
+    
+
+    const contaTestAtivo1 = new Conta('D0101', pessoa05);
+    const contaTestAtivo2 = new Conta('D0202', pessoa06);
     contaTestAtivo2.desativar();
-    const contaTestAtivo3 = new Conta('D0202');
+    const contaTestAtivo3 = new Conta('D0202', pessoa07);
     
     test('Deve desativar conta.', () => {
         contaTestAtivo1.desativar();
@@ -87,7 +108,9 @@ describe('Suite - Banco', () => {
     });
 
     // * alterar nome da conta
-    const contaTestAltararNome = new Conta('N0101');
+    const contato08 = new Contato('629558884500', 'fulano08@mail.com');
+    const pessoa08 = new PessoaFisica('Fulano de Tal-08', '123544888-46', contato08);
+    const contaTestAltararNome = new Conta('N0101', pessoa08);
 
     test('Deve lançar erro ao tentar alterar o nome da conta para o nome existente.', () => {
         expect(() => {
@@ -101,7 +124,10 @@ describe('Suite - Banco', () => {
         }).toThrow();
     });
 
-    const contaTestAltararNome2 = new Conta('N0102');
+    const contato09 = new Contato('629559994500', 'fulano09@mail.com');
+    const pessoa09 = new PessoaFisica('Fulano de Tal-09', '123999888-46', contato09);
+
+    const contaTestAltararNome2 = new Conta('N0102', pessoa09);
     test('Deve alterar nome com sucesso.', () => {
         let novoNome: string = 'N123';
         contaTestAltararNome2.alterarNomeConta(novoNome);
@@ -110,7 +136,7 @@ describe('Suite - Banco', () => {
     
     // * Deposito
     test('Deve realizar deposito com sucesso.', () => {
-        const conta = new Conta('0101');
+        const conta = new Conta('0101', pessoa01);
         conta.depositar(100);
         expect(100).toEqual(conta.saldo);
     });
@@ -128,7 +154,7 @@ describe('Suite - Banco', () => {
     });
 
     // * Sacar
-    const contaTestSaque = new Conta('0123');
+    const contaTestSaque = new Conta('0123', pessoa01);
     contaTestSaque.depositar(1000);
     
     test('Deve realizar saque com sucesso', () => {
@@ -153,9 +179,9 @@ describe('Suite - Banco', () => {
     });
 
     // * Transferência
-    const contaTestTransferencia_1 = new Conta('0123');
+    const contaTestTransferencia_1 = new Conta('0123', pessoa01);
     contaTestTransferencia_1.depositar(1000);
-    const contaTestTransferencia_2 = new Conta('0123');
+    const contaTestTransferencia_2 = new Conta('0123', pessoa02);
 
     test('Deve lançar error quando conta origem desativada tentar realizar transferência', () => {
         expect(() => {
@@ -182,7 +208,7 @@ describe('Suite - Banco', () => {
     });
 
     // * conceder cartao
-    const contaTestCartao01 = new Conta('C0101');
+    const contaTestCartao01 = new Conta('C0101', pessoa01);
     const cartaoTest01 = new Cartao(1, '1234-1234', '123');
     const cartaoTest02 = new Cartao(9, '4545-4545', '456');
     const cartaoTest03 = new Cartao(5, '7978-7978', '789');
